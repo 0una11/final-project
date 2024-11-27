6dc149eaa7ccb880671c5857cddf97c9563d7107
0una11 <0una110524@gmail.com>
2024-11-27 23:43:05 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Homepage from './Homepage';
+import BMICalculator from './BMICalculator';
+import Tracker from './Tracker';
+import FitnessSuggestions from './FitnessSuggestions';
+import Recipe from './Recipe';
+import WeeklyPlan from './WeeklyPlan';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        {/* Navigation bar */}
+        <nav>
+          <Link to="/">Home</Link> | 
+          <Link to="/bmi-calculator">BMI Calculator</Link> | 
+          <Link to="/tracker">Tracker</Link> | 
+          <Link to="/fitness-suggestions">Fitness Suggestions</Link> | 
+          <Link to="/recipe">Recipes for Health</Link> | 
+          <Link to="/weeklyplan">Weekly Workout Plan</Link>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/bmi-calculator" element={<BMICalculator />} />
+          <Route path="/tracker" element={<Tracker />} />
+          <Route path="/fitness-suggestions" element={<FitnessSuggestions />} />
+          <Route path="/recipe" element={<Recipe />} />
+          <Route path="/weeklyplan" element={<WeeklyPlan />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
